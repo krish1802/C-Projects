428e37ace0f18616d867d99b5533a0d0c6bc036e
krish1802 <69857495+krish1802@users.noreply.github.com>
2020-10-13 23:17:44 +0530

    Help For Arranging Name(For teachers)
    This C# project will help teachers to make lists of students with the help of alphabetical order.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Basic_C__Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell the total number of students");
+            int studentCount = Convert.ToInt32(Console.ReadLine());
+            string[] students = new string[studentCount];
+            Console.WriteLine("Type The Names of the students");
+            for (int i = 0; i < studentCount; i++)
+            {
+                students[i] = Console.ReadLine();
+            }
+            
+            Console.WriteLine("---------------------");
+
+            Array.Sort(students);
+
+
+
+            for (int i = 0; i < studentCount; i++)
+            {   
+                Console.WriteLine(students[i]);
+            }
+
+            Console.ReadKey();
+        
+        }
+    }
+}
